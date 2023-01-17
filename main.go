88f319d08790c669a0dd16e8e32687a4d1cfb627
navas <gabnavas.dev@gmail.com>
2023-01-17 16:29:41 -0300

    add docker, postgres database connection example

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5435
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "dev_database"
+)
+
 func main() {
-	println("hello world")
+	postgresStrConnection := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", postgresStrConnection)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	} else {
+		println("database is on!")
+	}
+	defer db.Close()
 }
